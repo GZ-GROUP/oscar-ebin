dd4634334d045a6e15ce04042b9be23de1205156
Javier Sánchesz <jadrian05.sanchez@gmail.com>
2026-04-22 19:35:06 -0500

    Section brand lista + imagenes

--- src/features/home/BrandsSection.jsx
+++ src/features/home/BrandsSection.jsx
@@ -1,8 +1,38 @@
+import React from 'react';
+import './BrandSecction.css';
+
+
 function BrandsSection() {
+    const brands = [
+        { id: 1, name: "Brand 1", image: "src/assets/tesla.png" },
+        { id: 2, name: "Brand 2", image: "src/assets/amazon.png" },
+        { id: 3, name: "Brand 3", image: "src/assets/nvidia.png" },
+        { id: 4, name: "Brand 4", image: "src/assets/google.png" },
+        { id: 5, name: "Brand 5", image: "src/assets/microsoft.png" },
+        { id: 6, name: "Brand 6", image: "src/assets/meta.png" },
+    ];
+
     return (
         <section className="brands-section">
+            <p className="brands-title">Empresas que confían en nosotros</p>
+            <div className="brands-grid">
+                {brands.map((brand) => (
+                    <div className="brand-circle" key={brand.id}>
+                        {brand.image ? (
+                            <img
+                                src={brand.image}
+                                alt={brand.name}
+                                className="brand-image"
+                            />
+                        ) : (
+                            
+                            <span className="brand-placeholder">{brand.id}</span>
+                        )}
+                    </div>
+                ))}
+            </div>
         </section>
-    )
+    );
 }
 
 export default BrandsSection;
